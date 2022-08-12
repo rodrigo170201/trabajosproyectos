046d0286fdcc0ab16d3bfacad2629bbf84b68d29
rodri <rodrimontero17@gmail.com>
2022-08-11 21:52:44 -0400

    iniciando 2

--- src/main/java/Proyecto1/consola.java
+++ src/main/java/Proyecto1/consola.java
@@ -2,6 +2,7 @@ package Proyecto1;
 
 public class consola {
     public static void main(String[] args) {
-
+        System.out.println("Hola mundo");
+        System.out.println("Esto es solo una prueba");
     }
 }
