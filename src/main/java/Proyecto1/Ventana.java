d2cbaadc6c32f0284f58a9c255324892fdcf1710
rodri <rodrimontero17@gmail.com>
2022-08-11 22:06:16 -0400

    iniciando 1

---
+++ src/main/java/Proyecto1/Ventana.java
@@ -0,0 +1,11 @@
+package Proyecto1;
+
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        setTitle("Proyecto 1");
+        setSize(400,400);
+        setVisible(true);
+    }
+}
--- src/main/java/Proyecto1/consola.java
+++ src/main/java/Proyecto1/consola.java
@@ -4,5 +4,6 @@ public class consola {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Esto es solo una prueba");
+        Ventana nueva = new Ventana();
     }
 }
